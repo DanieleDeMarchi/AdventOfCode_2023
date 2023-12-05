ddc9ecd3ae03807872883a0fc827f628915ea86c
demarchid <demarchid@previdom.previnet.it>
2023-12-05 16:22:23 +0100

    Day 3 part 2 completed

--- Day_3/Dart/main.dart
+++ Day_3/Dart/main.dart
@@ -2,20 +2,18 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  part1('../input1');
-//   part2('../input2');
+  // part1('../input1');
+  part2('../input2');
 }
 
 void part1(String inputFilePath) {
   final file = File(inputFilePath);
-
   List<String> lines = file.readAsLinesSync();
 
   int total = 0;
-
   for (var i = 0; i < lines.length; i++) {
     total += extractNumbers(lines[i], i)
-      .where((number) => isNearSymbol(number, lines))
+      .where((number) => isNearSymbol(number, lines, RegExp(r'[^0-9.*]')))
       .map((number) => number.value)
       .fold(0, (previousValue, element) => previousValue + element);
   }
@@ -37,7 +35,7 @@ List<Number> extractNumbers(String input, int rowNumber) {
     .toList();
 }
 
-bool isNearSymbol(Number number, List<String> lines) {
+bool isNearSymbol(Number number, List<String> lines, RegExp regex) {
   // print('Check number ${number.value}');
   // print('Number row ${number.row}');
   // print('startColumnIndex ${number.startColumnIndex} \nendColumnIndex ${number.endColumnIndex}');
@@ -49,13 +47,51 @@ bool isNearSymbol(Number number, List<String> lines) {
         continue;
       }
 
-      if(lines[i][j] != '.') {
+      if(regex.hasMatch(lines[i][j])) {
         // print('number ${number.value} is near symbol ${lines[i][j]}');
         return true;
       }
-
     }
   }
 
   return false;
 } 
+
+void part2(String inputFilePath) {
+  final file = File(inputFilePath);
+  List<String> lines = file.readAsLinesSync();
+  int total = calcGear(lines);
+  print('Part2 result: $total');
+}
+
+int calcGear(List<String> lines) {
+  var total = 0;
+  for (var i = 0; i < lines.length; i++) {
+    // find *
+    // get gear ratio of number near * (only if there are 2 numbers adjacent to *)
+    // add to total
+    total += RegExp(r'\*').allMatches(lines[i])
+      .map((match) => calculateGearRatio(lines, i, match.start))
+      .fold(0, (previousValue, element) => previousValue + element);
+  }
+  return total;
+}
+
+int calculateGearRatio(List<String> lines, int row, int column) {
+  print('asterisk found at $row $column');
+  // find number in rows [row-1, row+1] (inclusive)
+  List<Number> numbers = [];
+  for(var i = max(0, row -1); i <= min(lines.length, row +1); i++) {
+    numbers.addAll(extractNumbers(lines[i], i));
+  }
+
+  // keep only numbers that are in column range [column - 1, column + 1] (inclusive)
+  numbers = numbers.where((number) => number.endColumnIndex > column-1 && number.startColumnIndex <= column + 1 ).toList();
+  // if number of numbers == 2 multiply and return value
+  if(numbers.length == 2) {
+    return numbers[0].value * numbers[1].value;
+  }
+  // oteherwise return 0
+  return 0;
+
+}
