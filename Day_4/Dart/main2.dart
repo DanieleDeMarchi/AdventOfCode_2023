4b6092ba426712ba261e32daa86c097de199924f
Daniele De Marchi <demarchidaniele9@gmail.com>
2023-12-06 00:12:38 +0100

    Day 4 part 2 OK!

--- Day_4/Dart/main2.dart
+++ Day_4/Dart/main2.dart
@@ -2,41 +2,48 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  part1('../input1');
-  // part2('../input2');
+  part2('../input1');
 }
 
-void part1(String inputFilePath){
-  final file = File(inputFilePath);
-  final value = file
-      .readAsLinesSync()
-      .map((line) => calculateScoreOfLine(line))
-      .fold(0, (prev, cur) => prev + cur);
 
-  print('Part1 result: $value');
+void part2(String inputFilePath){
+  final file = File(inputFilePath);
+  List<Card> cards = file
+      .readAsLinesSync()
+      .map((line) => parseLine(line))
+      .toList();
+
+  for (var i = 0; i < cards.length; i++) {
+    int cardScore = cards[i].calcScore();
+    // print('Score of card $i: $cardScore');
+    for (var j = i+1; j < min(i+1+cardScore, cards.length) ; j++) {
+      // print('Adding one copy of card $j');
+      cards[j].copies += cards[i].copies;
+    }
+  }
+
+  final int total = cards.fold(0, (previousValue, element) => previousValue + element.copies);
+
+  print('Part2 result: $total');
 }
 
 
 class Card {
   Set<int> winningNumbers;
   List<int> myNumbers;
+  int score = 0;
+  int copies = 1;
 
   Card(this.winningNumbers, this.myNumbers);
 
   int calcScore() {
-    int winningCards = 0;
     for (var number in myNumbers) {
       if(winningNumbers.contains(number)){
-        winningCards++;
+        score++;
       }
-    }
-    return winningCards > 0 ? pow(2, winningCards-1).toInt() : 0;
+    } 
+    return this.score;
   }
-}
-
-int calculateScoreOfLine(String line) {
-  Card match = parseLine(line);
-  return match.calcScore();
 }
 
 Card parseLine(String line) {
